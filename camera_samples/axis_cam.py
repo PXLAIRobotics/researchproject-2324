4669fee484a698be8a56d108381f86ebcbe30e6b
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2024-02-20 14:34:17 +0100

    Added camera sample code & camera images

---
+++ camera_samples/axis_cam.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np
+import sys
+import argparse
+import requests
+from requests.auth import HTTPDigestAuth
+
+class Camera():
+
+    def __init__(self, params):
+        try:
+            # Set up the URL of the MJPEG stream
+            mjpeg_url = f"http://{params['url']}/{params['stream']}"
+
+            # Set up the username and password for authentication
+            username = params['name']
+            password = params['password']
+
+            # Make a GET request to the MJPEG stream with HTTP digest authentication
+            self.stream = requests.get(mjpeg_url, auth=HTTPDigestAuth(username, password), stream=True)
+
+            # Check if the request was successful (HTTP status code 200)
+            if self.stream.status_code == 200:
+                print("Ready for action!")
+            else:
+                print("Failed to retrieve MJPEG stream. HTTP status code:", stream.status_code)
+        except Exception as e:
+            print(e)
+            sys.exit()
+
+        print('Started connection on: ' + params['url'])
+
+    def show(self):
+        bytes = b''
+        # Read the MJPEG stream
+        for chunk in self.stream.iter_content(chunk_size=1024):
+            # Process the image bytes here as needed
+            bytes += chunk
+            a = bytes.find(b'\xff\xd8')
+            b = bytes.find(b'\xff\xd9')
+            if a != -1 and b != -1:
+                jpg = bytes[a:b + 2]
+                bytes = bytes[b + 2:]
+                frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+                cv2.imshow('Camera stream', frame)
+                if cv2.waitKey(1) == 27:
+                    exit(0)
+
+if __name__ == '__main__':
+    params = {
+        "url": "192.168.1.63/axis-cgi",
+        'name': "root",
+        'password': "7v5sgKiAlO9M",
+        "stream": "mjpg/video.cgi",
+        'fps': 30
+    }
+    cam = Camera(params)
+    cam.show()
